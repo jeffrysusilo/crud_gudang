2fbdaaf2f1896994ade44091028c5b83145ccb7a
Jeffry Susilo <93344979+jeffrysusilo@users.noreply.github.com>
2025-05-22 13:11:22 +0700

    Update db.js

--- inventory-node/models/db.js
+++ inventory-node/models/db.js
@@ -27,14 +27,14 @@
 
 // module.exports = connection;
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306
+  host: 'mysql',
+  user: 'root',
+  password: 'ZRfHhaDRreWOFhubIyxbqBwxYkWRnWWM',
+  database: 'railway',
+  port: 3306
 });
 
 connection.connect((err) => {
