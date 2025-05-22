9f46a277408ed697c078ac9b6a36edaca3fea97a
Jeffry Susilo <93344979+jeffrysusilo@users.noreply.github.com>
2025-05-22 08:41:01 +0700

    Update db.js

--- inventory-node/models/db.js
+++ inventory-node/models/db.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'inventory_db',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT || 3306
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log("Database connected");
+  console.log('Connected to MySQL!');
 });
 
 module.exports = connection;
